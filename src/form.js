8e767f38f9a3e23f8afda3072f7149e4b230764b
Camilo Del Valle Ledesma <camilodelvallel@gmail.com>
2023-01-23 13:19:38 -0500

    Fin de la programación del estado de boletas

--- src/form.js
+++ src/form.js
@@ -2,12 +2,18 @@ import React from "react";
 
 export default function Form(props) {
     const {movie} = props;
+    const [quantity, setQuantity] = React.useState(0);
+    // maxQuantity () {
+    //     if(movie.available==quantity){
+    //         return disabled
+    //     }
+    // }
     return (
         <form>
             <h3>{movie.name} {Form}</h3>
-            <button>-</button>
-            <p>0</p>
-            <button>+</button>
+            <button type="button"onClick={()=>setQuantity(quantity-1)} disabled={quantity <= 0} >-</button>
+            {quantity}
+            <button type="button" onClick={()=>setQuantity(quantity+1)} disabled={quantity >= movie.available}>+</button>
         </form>
-    )
+    );
 }
